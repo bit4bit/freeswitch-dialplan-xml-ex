d1acf096377c7ab88de48d21a2ec548484f39ce0
bit4bit <>
2022-03-19 16:29:46 +0000

    add conditional expressions
    FossilOrigin-Name: e156493e858983b6b1e0655654b382fe30ccb8d9ccc22d675b83973b544f6aa8

--- lib/freeswitch_dialplan_xml_ex.ex
+++ lib/freeswitch_dialplan_xml_ex.ex
@@ -9,6 +9,21 @@ defmodule FreeswitchDialplanXmlEx do
       Module.register_attribute(__MODULE__, :conditions, accumulate: true)
 
       import unquote(__MODULE__)
+
+      defp expression_from_vars_ast(ast) do
+        Macro.prewalker(ast)
+        |> Enum.map(& &1)
+        |> Enum.filter(fn
+          {field, {:<>, _, _}} -> true
+          {field, value} when is_binary(field) and is_binary(value) -> true
+          _rest -> false
+        end)
+        |> Enum.map(fn
+          {field, {:<>, _, [value | _]}} -> {field, "^#{value}.+$"}
+          {field, value} -> {field, "^#{value}$"}
+        end)
+        |> Map.new()
+      end
     end
   end
 
@@ -16,17 +31,31 @@ defmodule FreeswitchDialplanXmlEx do
     quote do
       def render(params) do
         @conditions
-        |> Enum.map(fn {extension, fun_condition} ->
+        |> Enum.map(fn {extension, fun_condition, vars_ast} ->
           case apply(__MODULE__, fun_condition, [params]) do
-            :ok ->
+            contents when is_binary(contents) ->
+              expression_of = expression_from_vars_ast(vars_ast)
+
               conditions =
-                Enum.map(params, fn {field, expression} ->
-                  ~s(<condition field="${#{field}}" expression="#{expression}">)
+                Enum.map(params, fn
+                  {field, _values} ->
+                    expression = expression_of[field]
+
+                    ~s(<condition field="${#{field}}" expression="#{expression}")
+                end)
+                |> Enum.reverse()
+                |> then(fn
+                  [last | rest] ->
+                    rest
+                    |> Enum.map(&(&1 <> "/>"))
+                    |> Enum.join("")
+                    # append main condition
+                    |> Kernel.<>(last <> ">")
                 end)
 
-              ~s(<extension name="#{extension}">#{conditions}</condition></extension>)
+              ~s(<extension name="#{extension}">#{conditions}#{contents}</condition></extension>)
 
-            :not_found ->
+            _ ->
               ""
           end
         end)
@@ -35,6 +64,7 @@ defmodule FreeswitchDialplanXmlEx do
     end
   end
 
+  # TAG: <extension..
   defmacro extension(name, do: block) do
     quote do
       @extensions unquote(name)
@@ -43,21 +73,16 @@ defmodule FreeswitchDialplanXmlEx do
     end
   end
 
-  defmacro condition(var, contents) do
+  # TAG: <condition..
+  defmacro condition(var, do: block) do
     # TOMANDO DE: ExUnit.Case
+    # must returns contents of <condition>..</condition>
     contents =
-      case contents do
-        [do: block] ->
-          quote do
-            unquote(block)
-            :ok
-          end
-
-        _ ->
-          quote do
-            try(unquote(contents))
-            :ok
-          end
+      quote do
+        case unquote(block) do
+          nil -> ""
+          rest -> rest
+        end
       end
 
     var = Macro.escape(var)
@@ -66,10 +91,10 @@ defmodule FreeswitchDialplanXmlEx do
     fun_name = make_ref() |> :erlang.term_to_binary() |> Base.encode16()
 
     quote bind_quoted: [fun_name: fun_name, var: var, contents: contents] do
-      extension = List.last(@extensions)
+      extension = List.first(@extensions)
       name = "#{extension}-condition-#{fun_name}" |> String.to_atom()
 
-      @conditions {extension, name}
+      @conditions {extension, name, var}
       def unquote(name)(unquote(var)), do: unquote(contents)
       def unquote(name)(_), do: :not_found
     end
--- test/freeswitch_dialplan_xml_ex_test.exs
+++ test/freeswitch_dialplan_xml_ex_test.exs
@@ -4,16 +4,36 @@ defmodule FreeswitchDialplanXmlExTest do
   defmodule BasicDialplan do
     use FreeswitchDialplanXmlEx
 
-    extension "echo" do
+    extension "equal expression" do
       condition %{"test" => "123"} do
+      end
+    end
+
+    extension "concatenation expression" do
+      condition %{"test" => "12" <> _rest} do
+      end
+    end
+
+    extension "multiple conditions" do
+      condition %{"A" => "1", "B" => "2"} do
       end
     end
 
     build()
   end
 
-  test "dialplan with empty extension" do
-    assert BasicDialplan.render(%{"test" => "123"}) ==
-             ~s(<extension name="echo"><condition field="${test}" expression="123"></condition></extension>)
+  test "multiple conditions" do
+    assert BasicDialplan.render(%{"A" => "1", "B" => "2"}) =~
+             ~s(<extension name="multiple conditions"><condition field="${A}" expression="^1$"/><condition field="${B}" expression="^2$"></condition></extension>)
+  end
+
+  test "dialplan equal expresion" do
+    assert BasicDialplan.render(%{"test" => "123"}) =~
+             ~s(<extension name="equal expression"><condition field="${test}" expression="^123$"></condition></extension>)
+  end
+
+  test "dialplan with concatenation expression" do
+    assert BasicDialplan.render(%{"test" => "123"}) =~
+             ~s(<extension name="concatenation expression"><condition field="${test}" expression="^12.+$"></condition></extension>)
   end
 end
